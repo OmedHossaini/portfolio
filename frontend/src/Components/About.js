d821e795404f5e74cbb0b722703c6c9d911bbd12
OmedHossaini <omedhossainiomedhossaini123@hotmail.com>
2023-12-07 12:12:51 -0500

    about me is more readable

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -13,16 +13,16 @@ function About() {
         <div>
           <h2 className="about__subtitle">I'm Omed</h2>
           <p className="about__text">Welcome to my world! I am a passionate full-stack web developer, dedicated to crafting beautiful and functional web applications. My skill set includes HTML, CSS, JavaScript, Node.js, MongoDB, and React.</p>
-
+<br></br>
           <p className="about__text">Outside of coding, I enjoy a variety of hobbies that keep me balanced and inspired. When I'm not immersed in the world of web development, you can find me:</p>
-
+<br></br>
           <ul className="about__list">
             <li><i className='bx bx-right-arrow-circle'></i> Playing video games <i className='bx bxs-game bx-fade-up'></i></li>
             <li><i className='bx bx-right-arrow-circle'></i> Shooting hoops on the basketball court <i className='bx bxs-basketball bx-tada'></i></li>
             <li><i className='bx bx-right-arrow-circle'></i> Watching YouTube videos <i className='bx bxs-videos bx-flashing'></i></li>
             <li><i className='bx bx-right-arrow-circle'></i> Expressing my creativity through drawing <i className='bx bx-pencil bx-burst'></i></li>
           </ul>
-
+<br></br>
           <p className="about__text">
             I believe that a diverse set of interests fuels creativity and helps me approach coding challenges with a fresh perspective. Let's build something amazing together!
           </p>
